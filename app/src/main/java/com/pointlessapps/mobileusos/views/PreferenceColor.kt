7f2823be21124744e34e850a212c04a95191250a
KonradWrbl <160815@stud.prz.edu.pl>
2020-03-15 23:07:51 +0100

    'color_picker_part_1'

--- app/src/main/java/com/pointlessapps/mobileusos/views/PreferenceColor.kt
+++ app/src/main/java/com/pointlessapps/mobileusos/views/PreferenceColor.kt
@@ -6,21 +6,35 @@ import android.view.ViewGroup
 import androidx.preference.Preference
 import com.pointlessapps.mobileusos.R
 import com.pointlessapps.mobileusos.utils.DialogUtil
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.content.ContextCompat
+import org.jetbrains.anko.colorAttr
 
 class PreferenceColor(context: Context, attrs: AttributeSet) : Preference(context, attrs) {
 
 	init {
 //		Proponuję zrobić w taki sposób, że ikona to będzie plik ic_circle.xml, i po zmianie koloru wysatrczy zmienić tint dla ikony
-//		setIcon(R.drawable.ic_circle)
-//		icon.setTint(Color.parseColor("#123123"))
+		setIcon(R.drawable.ic_circle)
+		val color = getPersistedInt(ContextCompat.getColor(context, R.color.color1))
+		icon.setTint(color)
 //		kolory zapisujesz w sharedPreferences
 //		i będzie to coś w style persistInt(kolor) i getPersistedInt(domyslny_kolor)
 	}
 
 	override fun onClick() {
 //		Tutaj po kliknięciu trzeba wyświetlić dialog z możliwością wyboru koloru
-//		DialogUtil.create(context, R.layout.dialog_color_picker, { dialog ->
+		DialogUtil.create(context, R.layout.dialog_color_picker, { dialog ->
 //          Tutaj piszesz kod, który będzie zapisywał wybrany kolor itd
-//		}, DialogUtil.UNDEFINED_WINDOW_SIZE, ViewGroup.LayoutParams.WRAP_CONTENT)
+			for(i in 1..20) {
+				val view = dialog.findViewById<AppCompatImageView>(context.resources.getIdentifier("imageColor$i", "id", context.packageName));
+				view.setOnClickListener{
+					val color = view.imageTintList?.defaultColor!!;
+					persistInt(color);
+					icon.setTint(color)
+					dialog.hide()
+				}
+			}
+
+		}, DialogUtil.UNDEFINED_WINDOW_SIZE, ViewGroup.LayoutParams.WRAP_CONTENT)
 	}
 }
